f46ceff4bf7337881f4053ec7041c84840f0f96b
James-kirk1 <oliver_hands@yahoo.co.uk>
2019-02-21 19:14:24 +0000

    more errors

---
+++ Warhammer 40000 sim/NewFolder1/Unused/Chaos_Space_Marine_Chapter_Form.vb
@@ -0,0 +1,103 @@
+﻿'Public Class Chaos_Space_Marine_Chapter_Form
+
+'    Private Sub CSM_Worldeaters_Click(sender As System.Object, e As System.EventArgs) Handles CSM_Worldeaters.Click
+'        Select Case Race_Selection_Form.Playerid
+'            Case Is = 1
+'                Race_Selection_Form.playeronerace = 26
+'            Case Is = 2
+'                Race_Selection_Form.playertworace = 26
+'            Case Is = 3
+'                Race_Selection_Form.playerthreerace = 26
+'            Case Is = 4
+'                Race_Selection_Form.playerfourrace = 26
+'            Case Is = 5
+'                Race_Selection_Form.playerfiverace = 26
+'            Case Is = 6
+'                Race_Selection_Form.playersixrace = 26
+'            Case Is = 7
+'                Race_Selection_Form.playersevenrace = 26
+'            Case Is = 8
+'                Race_Selection_Form.playereightrace = 26
+'        End Select
+'        ' RaceSelectiontoTeamSelection()
+'    End Sub
+
+'    Private Sub CSM_Thousand_Sons_Click(sender As System.Object, e As System.EventArgs) Handles CSM_Thousand_Sons.Click
+'        Select Case Race_Selection_Form.Playerid
+'            Case Is = 1
+'                Race_Selection_Form.playeronerace = 27
+'            Case Is = 2
+'                Race_Selection_Form.playertworace = 27
+'            Case Is = 3
+'                Race_Selection_Form.playerthreerace = 27
+'            Case Is = 4
+'                Race_Selection_Form.playerfourrace = 27
+'            Case Is = 5
+'                Race_Selection_Form.playerfiverace = 27
+'            Case Is = 6
+'                Race_Selection_Form.playersixrace = 27
+'            Case Is = 7
+'                Race_Selection_Form.playersevenrace = 27
+'            Case Is = 8
+'                Race_Selection_Form.playereightrace = 27
+'        End Select
+'        'Functions.finalplayerthentoteamsetup()
+
+'    End Sub
+
+'    Private Sub CSM_Death_Guard_Click(sender As System.Object, e As System.EventArgs) Handles CSM_Death_Guard.Click
+'        Select Case Race_Selection_Form.Playerid
+'            Case Is = 1
+'                Race_Selection_Form.playeronerace = 28
+'            Case Is = 2
+'                Race_Selection_Form.playertworace = 28
+'            Case Is = 3
+'                Race_Selection_Form.playerthreerace = 28
+'            Case Is = 4
+'                Race_Selection_Form.playerfourrace = 28
+'            Case Is = 5
+'                Race_Selection_Form.playerfiverace = 28
+'            Case Is = 6
+'                Race_Selection_Form.playersixrace = 28
+'            Case Is = 7
+'                Race_Selection_Form.playersevenrace = 28
+'            Case Is = 8
+'                Race_Selection_Form.playereightrace = 28
+'        End Select
+'        ' Functions.finalplayerthentoteamsetup()
+'    End Sub
+
+'    Private Sub CSM_Emperors_Children_Click(sender As System.Object, e As System.EventArgs) Handles CSM_Emperors_Children.Click
+'        Select Case Race_Selection_Form.Playerid
+'            Case Is = 1
+'                Race_Selection_Form.playeronerace = 29
+'            Case Is = 2
+'                Race_Selection_Form.playertworace = 29
+'            Case Is = 3
+'                Race_Selection_Form.playerthreerace = 29
+'            Case Is = 4
+'                Race_Selection_Form.playerfourrace = 29
+'            Case Is = 5
+'                Race_Selection_Form.playerfiverace = 29
+'            Case Is = 6
+'                Race_Selection_Form.playersixrace = 29
+'            Case Is = 7
+'                Race_Selection_Form.playersevenrace = 29
+'            Case Is = 8
+'                Race_Selection_Form.playereightrace = 29
+'        End Select
+'        'Functions.finalplayerthentoteamsetup()
+'    End Sub
+
+'    Private Sub Returntoraceselection_Click(sender As System.Object, e As System.EventArgs) Handles Returntoraceselection.Click
+'        Race_Selection_Form.Show()
+'        Race_Selection_Form.BringToFront()
+'        Me.Hide()
+'        Me.SendToBack()
+'    End Sub
+
+'    Private Sub Chaos_Space_Marine_Chapter_Form_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+'        Me.FormBorderStyle = Windows.Forms.FormBorderStyle.None
+'        Me.StartPosition = FormStartPosition.CenterScreen
+'    End Sub
+'End Class
---
+++ Warhammer 40000 sim/NewFolder1/Unused/Space_Marine_Chapter_Form.Designer.vb
@@ -0,0 +1,218 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Space_Marine_Chapter_Form
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.SM_Ultramarines = New System.Windows.Forms.Button()
+        Me.SM_Imperial_Fists = New System.Windows.Forms.Button()
+        Me.SM_Crimson_Fists = New System.Windows.Forms.Button()
+        Me.SM_Black_Templars = New System.Windows.Forms.Button()
+        Me.SM_Raven_Guard = New System.Windows.Forms.Button()
+        Me.SM_Salamanders = New System.Windows.Forms.Button()
+        Me.SM_White_Scars = New System.Windows.Forms.Button()
+        Me.SM_LotD = New System.Windows.Forms.Button()
+        Me.SM_Blood_Angels = New System.Windows.Forms.Button()
+        Me.SM_Flesh_Tearers = New System.Windows.Forms.Button()
+        Me.SM_Dark_Angels = New System.Windows.Forms.Button()
+        Me.SM_Space_Wolves = New System.Windows.Forms.Button()
+        Me.SM_Deathwatch = New System.Windows.Forms.Button()
+        Me.SM_Grey_Knights = New System.Windows.Forms.Button()
+        Me.Returntoraceselection = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'SM_Ultramarines
+        '
+        Me.SM_Ultramarines.Location = New System.Drawing.Point(12, 12)
+        Me.SM_Ultramarines.Name = "SM_Ultramarines"
+        Me.SM_Ultramarines.Size = New System.Drawing.Size(210, 23)
+        Me.SM_Ultramarines.TabIndex = 0
+        Me.SM_Ultramarines.Text = "Ultramarines"
+        Me.SM_Ultramarines.UseVisualStyleBackColor = True
+        '
+        'SM_Imperial_Fists
+        '
+        Me.SM_Imperial_Fists.Location = New System.Drawing.Point(12, 42)
+        Me.SM_Imperial_Fists.Name = "SM_Imperial_Fists"
+        Me.SM_Imperial_Fists.Size = New System.Drawing.Size(210, 23)
+        Me.SM_Imperial_Fists.TabIndex = 1
+        Me.SM_Imperial_Fists.Text = "Imperial Fists"
+        Me.SM_Imperial_Fists.UseVisualStyleBackColor = True
+        '
+        'SM_Crimson_Fists
+        '
+        Me.SM_Crimson_Fists.Location = New System.Drawing.Point(12, 71)
+        Me.SM_Crimson_Fists.Name = "SM_Crimson_Fists"
+        Me.SM_Crimson_Fists.Size = New System.Drawing.Size(210, 23)
+        Me.SM_Crimson_Fists.TabIndex = 2
+        Me.SM_Crimson_Fists.Text = "Crimson Fists"
+        Me.SM_Crimson_Fists.UseVisualStyleBackColor = True
+        '
+        'SM_Black_Templars
+        '
+        Me.SM_Black_Templars.Location = New System.Drawing.Point(12, 100)
+        Me.SM_Black_Templars.Name = "SM_Black_Templars"
+        Me.SM_Black_Templars.Size = New System.Drawing.Size(210, 23)
+        Me.SM_Black_Templars.TabIndex = 3
+        Me.SM_Black_Templars.Text = "Black Templars"
+        Me.SM_Black_Templars.UseVisualStyleBackColor = True
+        '
+        'SM_Raven_Guard
+        '
+        Me.SM_Raven_Guard.Location = New System.Drawing.Point(12, 129)
+        Me.SM_Raven_Guard.Name = "SM_Raven_Guard"
+        Me.SM_Raven_Guard.Size = New System.Drawing.Size(210, 23)
+        Me.SM_Raven_Guard.TabIndex = 4
+        Me.SM_Raven_Guard.Text = "Raven Guard"
+        Me.SM_Raven_Guard.UseVisualStyleBackColor = True
+        '
+        'SM_Salamanders
+        '
+        Me.SM_Salamanders.Location = New System.Drawing.Point(12, 158)
+        Me.SM_Salamanders.Name = "SM_Salamanders"
+        Me.SM_Salamanders.Size = New System.Drawing.Size(210, 23)
+        Me.SM_Salamanders.TabIndex = 5
+        Me.SM_Salamanders.Text = "Salamanders"
+        Me.SM_Salamanders.UseVisualStyleBackColor = True
+        '
+        'SM_White_Scars
+        '
+        Me.SM_White_Scars.Location = New System.Drawing.Point(12, 187)
+        Me.SM_White_Scars.Name = "SM_White_Scars"
+        Me.SM_White_Scars.Size = New System.Drawing.Size(210, 23)
+        Me.SM_White_Scars.TabIndex = 6
+        Me.SM_White_Scars.Text = "White Scars"
+        Me.SM_White_Scars.UseVisualStyleBackColor = True
+        '
+        'SM_LotD
+        '
+        Me.SM_LotD.Location = New System.Drawing.Point(12, 216)
+        Me.SM_LotD.Name = "SM_LotD"
+        Me.SM_LotD.Size = New System.Drawing.Size(210, 23)
+        Me.SM_LotD.TabIndex = 7
+        Me.SM_LotD.Text = "Legion of the Damned"
+        Me.SM_LotD.UseVisualStyleBackColor = True
+        '
+        'SM_Blood_Angels
+        '
+        Me.SM_Blood_Angels.Location = New System.Drawing.Point(12, 245)
+        Me.SM_Blood_Angels.Name = "SM_Blood_Angels"
+        Me.SM_Blood_Angels.Size = New System.Drawing.Size(210, 23)
+        Me.SM_Blood_Angels.TabIndex = 8
+        Me.SM_Blood_Angels.Text = "Blood Angels"
+        Me.SM_Blood_Angels.UseVisualStyleBackColor = True
+        '
+        'SM_Flesh_Tearers
+        '
+        Me.SM_Flesh_Tearers.Location = New System.Drawing.Point(12, 274)
+        Me.SM_Flesh_Tearers.Name = "SM_Flesh_Tearers"
+        Me.SM_Flesh_Tearers.Size = New System.Drawing.Size(210, 23)
+        Me.SM_Flesh_Tearers.TabIndex = 9
+        Me.SM_Flesh_Tearers.Text = "Flesh Tearers"
+        Me.SM_Flesh_Tearers.UseVisualStyleBackColor = True
+        '
+        'SM_Dark_Angels
+        '
+        Me.SM_Dark_Angels.Location = New System.Drawing.Point(12, 303)
+        Me.SM_Dark_Angels.Name = "SM_Dark_Angels"
+        Me.SM_Dark_Angels.Size = New System.Drawing.Size(210, 23)
+        Me.SM_Dark_Angels.TabIndex = 10
+        Me.SM_Dark_Angels.Text = "Dark Angels"
+        Me.SM_Dark_Angels.UseVisualStyleBackColor = True
+        '
+        'SM_Space_Wolves
+        '
+        Me.SM_Space_Wolves.Location = New System.Drawing.Point(12, 332)
+        Me.SM_Space_Wolves.Name = "SM_Space_Wolves"
+        Me.SM_Space_Wolves.Size = New System.Drawing.Size(210, 23)
+        Me.SM_Space_Wolves.TabIndex = 11
+        Me.SM_Space_Wolves.Text = "Space Wolves"
+        Me.SM_Space_Wolves.UseVisualStyleBackColor = True
+        '
+        'SM_Deathwatch
+        '
+        Me.SM_Deathwatch.Location = New System.Drawing.Point(12, 360)
+        Me.SM_Deathwatch.Name = "SM_Deathwatch"
+        Me.SM_Deathwatch.Size = New System.Drawing.Size(210, 23)
+        Me.SM_Deathwatch.TabIndex = 12
+        Me.SM_Deathwatch.Text = "Deathwatch"
+        Me.SM_Deathwatch.UseVisualStyleBackColor = True
+        '
+        'SM_Grey_Knights
+        '
+        Me.SM_Grey_Knights.Location = New System.Drawing.Point(12, 389)
+        Me.SM_Grey_Knights.Name = "SM_Grey_Knights"
+        Me.SM_Grey_Knights.Size = New System.Drawing.Size(210, 23)
+        Me.SM_Grey_Knights.TabIndex = 13
+        Me.SM_Grey_Knights.Text = "Grey Knights"
+        Me.SM_Grey_Knights.UseVisualStyleBackColor = True
+        '
+        'Returntoraceselection
+        '
+        Me.Returntoraceselection.Location = New System.Drawing.Point(12, 447)
+        Me.Returntoraceselection.Name = "Returntoraceselection"
+        Me.Returntoraceselection.Size = New System.Drawing.Size(210, 23)
+        Me.Returntoraceselection.TabIndex = 15
+        Me.Returntoraceselection.Text = "Return to Race Selection"
+        Me.Returntoraceselection.UseVisualStyleBackColor = True
+        '
+        'Space_Marine_Chapter_Form
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(234, 550)
+        Me.Controls.Add(Me.Returntoraceselection)
+        Me.Controls.Add(Me.SM_Grey_Knights)
+        Me.Controls.Add(Me.SM_Deathwatch)
+        Me.Controls.Add(Me.SM_Space_Wolves)
+        Me.Controls.Add(Me.SM_Dark_Angels)
+        Me.Controls.Add(Me.SM_Flesh_Tearers)
+        Me.Controls.Add(Me.SM_Blood_Angels)
+        Me.Controls.Add(Me.SM_LotD)
+        Me.Controls.Add(Me.SM_White_Scars)
+        Me.Controls.Add(Me.SM_Salamanders)
+        Me.Controls.Add(Me.SM_Raven_Guard)
+        Me.Controls.Add(Me.SM_Black_Templars)
+        Me.Controls.Add(Me.SM_Crimson_Fists)
+        Me.Controls.Add(Me.SM_Imperial_Fists)
+        Me.Controls.Add(Me.SM_Ultramarines)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Space_Marine_Chapter_Form"
+        Me.Text = "Space Marine Chapters"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents SM_Ultramarines As System.Windows.Forms.Button
+    Friend WithEvents SM_Imperial_Fists As System.Windows.Forms.Button
+    Friend WithEvents SM_Crimson_Fists As System.Windows.Forms.Button
+    Friend WithEvents SM_Black_Templars As System.Windows.Forms.Button
+    Friend WithEvents SM_Raven_Guard As System.Windows.Forms.Button
+    Friend WithEvents SM_Salamanders As System.Windows.Forms.Button
+    Friend WithEvents SM_White_Scars As System.Windows.Forms.Button
+    Friend WithEvents SM_LotD As System.Windows.Forms.Button
+    Friend WithEvents SM_Blood_Angels As System.Windows.Forms.Button
+    Friend WithEvents SM_Flesh_Tearers As System.Windows.Forms.Button
+    Friend WithEvents SM_Dark_Angels As System.Windows.Forms.Button
+    Friend WithEvents SM_Space_Wolves As System.Windows.Forms.Button
+    Friend WithEvents SM_Deathwatch As System.Windows.Forms.Button
+    Friend WithEvents SM_Grey_Knights As System.Windows.Forms.Button
+    Friend WithEvents Returntoraceselection As System.Windows.Forms.Button
+End Class
